6242cff74a71c823a4ae3024d4af10371100295e
Ferrerov <valenferrero01@gmail.com>
2024-04-11 11:45:21 -0300

    carga de ej 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+  FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'clase_1';
+
+  protected edadUno: number = 0;
+  protected edadDos: number = 0;
+  protected suma: number = 0;
+  protected promedio: number = 0;
+
+  protected onBtn()
+  {
+    //this.title="hola";
+  }
+
+  protected calcularEdadYPromedio()
+  {
+    this.suma = this.edadUno + this.edadDos;
+    this.promedio = this.suma / 2;
+  }
 }
+ 
